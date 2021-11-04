c97f4b51afff99dac52badbccd4380440cf142cd
danie.august.j <Danie.August.J@gmail.com>
2021-11-04 15:23:12 -0500

    Basic searching for states working have to swap to page using state.changeState('searchScreen'); in console window.

--- Gather/js/state.js
+++ Gather/js/state.js
@@ -2,10 +2,15 @@ class PageState{
 
     constructor(startingState){
         this.currentState = startingState;
+        this.hidePages(startingState);
     }
 
-    changeState(newState){
+    changeState = (newState)=>{
         this.currentState = newState;
+        this.hidePages(newState);
+    }
+
+    hidePages = (newState)=>{
         document.querySelectorAll('.page').forEach(page => {
             if(page.id != newState)
                 page.classList.add('hidden');
